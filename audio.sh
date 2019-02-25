8762deaeb105c13e19225e05d19d8bd92ada1e0a
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-02-24 19:06:13 -0600

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#Soma FM
+git clone git@github.com:rockymadden/somafm-cli.git
+cd somafm-cli
+make
+make install
 sudo npm install --global somafm
+
+#Other Stuff
 sudo apt update && sudo apt install -y audacity picard
