85530d4b649742210c1cc0d1a13b34fb80cb33a2
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-02-24 19:22:34 -0600

    Update audio.sh

--- audio.sh
+++ audio.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #Soma FM
+sudo apt update && sudo apt install -y nodejs yarn
+sudo npm init ava
+cd ~/git
 git clone git@github.com:rockymadden/somafm-cli.git
 cd somafm-cli
 make
@@ -8,4 +11,4 @@ make install
 sudo npm install --global somafm
 
 #Other Stuff
-sudo apt update && sudo apt install -y audacity picard
+sudo apt update && sudo apt install -y audacity mplayer mpv picard streamripper 
