d1fc0228860e5179a22899c02b3251b84bf595ca
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-03-28 18:21:53 -0500

    Update kicad.sh

--- kicad.sh
+++ kicad.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y kicad extra-xdg-menus kicad-doc-en kicad-packages3d
+sudo add-apt-repository ppa:js-reynaud/kicad-5.1
+sudo apt update && sudo apt install kicad extra-xdg-menus kicad-doc-en kicad-packages3d kicad-libraries kicad-symbols kicad-templates kicad-footprints
