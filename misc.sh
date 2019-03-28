15303475724539cd7ee3a9382dd373c469dee866
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-03-28 18:40:12 -0500

    Update misc.sh

--- misc.sh
+++ misc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt update && sudo apt install neofetch tilix tilix-common 
-sudo apt update && sudo apt install gnome-builder gvfs-backends font-manager
+sudo apt update && sudo apt install gvfs-backends font-manager
 sudo apt update && sudo apt install inkscape 
