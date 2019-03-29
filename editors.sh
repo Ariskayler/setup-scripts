3aeaff7496fa2c5ff6c98871a7adb9e64592bfd2
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-03-29 13:04:25 -0500

    Create editors.sh

---
+++ editors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Atom:
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt update && sudo apt install atom
+
+#Sublime Text:
+sudo apt update && sudo apt install sublime-text
