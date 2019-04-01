59772bce094ba00ec01e2334107c700f0883a6a9
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-04-01 12:02:03 -0500

    Update editors.sh

--- editors.sh
+++ editors.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+#Python:
+sudo apt update && sudo apt install python3 pip3 
+
 #Atom:
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt update && sudo apt install atom
+sudo apt update && sudo apt install atom 
+pip3 install pycodestyle
+#Install linter linter-pycodestyle Hydrogen inside Atom.
 
 #Sublime Text:
 sudo apt update && sudo apt install sublime-text
