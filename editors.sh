68ae9814dc7b9f9ad7302243f516bbcb9b3ce7ed
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-04-09 11:54:13 -0500

    Update editors.sh

--- editors.sh
+++ editors.sh
@@ -6,9 +6,7 @@ sudo apt update && sudo apt install python3 pip3
 #Atom:
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt update && sudo apt install atom pycodestyle
-pip3 install pycodestyle
-#Install linter linter-pycodestyle Hydrogen inside Atom.
+sudo apt update && sudo apt install atom
 
-#Sublime Text:
-sudo apt update && sudo apt install sublime-text
+#Install (linter Hydrogen atom-beautify color-picker pigments) inside Atom.
+
