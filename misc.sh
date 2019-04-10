ffe92798908f3e9a22e4df1cf48c4b3ef536a06b
Ariskayler <47371579+Ariskayler@users.noreply.github.com>
2019-04-10 17:36:03 -0500

    Update misc.sh

--- misc.sh
+++ misc.sh
@@ -3,3 +3,4 @@
 sudo apt update && sudo apt install neofetch tilix tilix-common 
 sudo apt update && sudo apt install gvfs-backends font-manager
 sudo apt update && sudo apt install inkscape 
+sudo apt update && sudo apt install conky xmms2 moc audacious moc-ffmpeg-plugin
